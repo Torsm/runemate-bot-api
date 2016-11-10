2e5a3bf577d50a0adf381b250635fb2c3d04e1d7
Torben <torben.schmitz@outlook.de>
2016-11-10 01:58:03 +0100

    Fixed IndependentLoopingThread

--- src/net/celestialproductions/api/bot/framework/IndependentLoopingThread.java
+++ src/net/celestialproductions/api/bot/framework/IndependentLoopingThread.java
@@ -3,8 +3,6 @@ package net.celestialproductions.api.bot.framework;
 import com.runemate.game.api.hybrid.Environment;
 import com.runemate.game.api.script.Execution;
 import com.runemate.game.api.script.framework.AbstractBot;
-import com.runemate.game.api.script.framework.AbstractScript;
-import com.runemate.game.api.script.framework.core.LoopingThread;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,39 +13,34 @@ import java.util.concurrent.TimeUnit;
  * @author Savior
  */
 public class IndependentLoopingThread extends Thread {
+    private final AbstractBot bot;
     private final Runnable runnable;
     private final long frequency;
 
     public IndependentLoopingThread(final String name, final Runnable runnable, final long frequency) {
-        this(name, runnable, frequency, TimeUnit.MILLISECONDS);
+        this(Environment.getBot(), name, runnable, frequency, TimeUnit.MILLISECONDS);
     }
 
     public IndependentLoopingThread(final String name, final Runnable runnable, final long frequency, final TimeUnit timeUnit) {
+        this(Environment.getBot(), name, runnable, frequency, timeUnit);
+    }
+
+    public IndependentLoopingThread(final AbstractBot bot, final String name, final Runnable runnable, final long frequency) {
+        this(bot, name, runnable, frequency, TimeUnit.MILLISECONDS);
+    }
+
+    public IndependentLoopingThread(final AbstractBot bot, final String name, final Runnable runnable, final long frequency, final TimeUnit timeUnit) {
         super(name);
+        this.bot = bot;
         this.runnable = runnable;
         this.frequency = timeUnit.toMillis(frequency);
     }
 
     @Override
     public void run() {
-        while (!interrupted()) {
+        while (!interrupted() && bot.getState() != AbstractBot.State.STOPPED) {
             runnable.run();
             Execution.delay(frequency);
         }
     }
-
-    //TODO revert once bug is fixed (Environment.getBot() throwing NPE)
-
-    //@Override
-    //public void run() {
-    //    final AbstractBot bot = Environment.getBot();
-    //    if (bot != null) {
-    //        while (!interrupted() && bot.getState() != AbstractBot.State.STOPPED) {
-    //            runnable.run();
-    //            Execution.delay(frequency);
-    //        }
-    //    } else {
-    //        throw new IllegalStateException("Bot is null");
-    //    }
-    //}
 }
--- src/net/celestialproductions/api/game/entities/AbstractMapper.java
+++ src/net/celestialproductions/api/game/entities/AbstractMapper.java
@@ -1,28 +1,36 @@
 package net.celestialproductions.api.game.entities;
 
+import com.runemate.game.api.script.framework.AbstractBot;
+
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Abstract version of a mapper that can be used to attach timeable entities into a collection
  *
  * @author Defeat3d
- *
  * @param <T> type of timeable entity to map (trees, spots, rocks)
  */
 public abstract class AbstractMapper<T extends TimeableEntity> implements Runnable {
+    private final AbstractBot bot;
     private final TimeableEntityCollection<T> collection;
 
     /**
      * Constructs a mapper
      * @param collection the collection to populate using this mapper
      */
-    public AbstractMapper(final TimeableEntityCollection<T> collection) {
+    public AbstractMapper(final AbstractBot bot, final TimeableEntityCollection<T> collection) {
+        this.bot = bot;
         this.collection = collection;
     }
 
     @Override
     public void run() {
-        map().stream().filter(e -> !collection.getCoordinates().contains(e.getPosition())).forEach(collection::add);
+        try {
+            bot.getPlatform().invokeAndWait(() -> map()).stream().filter(e -> !collection.getCoordinates().contains(e.getPosition())).forEach(collection::add);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     /**
