b8da69c5524d6feb277ad8d1be47e2cdfe88a425
Torben <torben.schmitz@outlook.de>
2016-11-10 01:57:13 +0100

    Added extension for TreeBot

---
+++ src/net/celestialproductions/api/bot/framework/tree/ExtendedTreeBot.java
@@ -0,0 +1,49 @@
+package net.celestialproductions.api.bot.framework.tree;
+
+import com.runemate.game.api.script.framework.tree.TreeBot;
+import com.runemate.game.api.script.framework.tree.TreeTask;
+
+/**
+ * @author Savior
+ */
+public class ExtendedTreeBot extends TreeBot {
+    private volatile TreeTask tree;
+
+    @Override
+    public final TreeTask createRootTask() {
+        return new RootTask();
+    }
+
+    public void setTree(final TreeTask root) {
+        this.tree = root;
+    }
+
+    private class RootTask extends TreeTask {
+        @Override
+        public boolean isLeaf() {
+            return tree == null;
+        }
+
+        @Override
+        public TreeTask failureTask() {
+            return tree.failureTask();
+        }
+
+        @Override
+        public void execute() {
+            if (tree != null) {
+                tree.execute();
+            }
+        }
+
+        @Override
+        public TreeTask successTask() {
+            return tree.successTask();
+        }
+
+        @Override
+        public boolean validate() {
+            return tree.validate();
+        }
+    }
+}
