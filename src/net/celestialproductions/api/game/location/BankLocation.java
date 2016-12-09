eb0b2f98f8c50604dfb686c1d2d4a34f79014eee
DerkSchooltink <derk.schooltink@gmail.com>
2016-12-09 20:39:52 +0100

    Added locations to BankLocation
    Added different bankinglocations to the banklocation enum

--- src/net/celestialproductions/api/game/location/BankLocation.java
+++ src/net/celestialproductions/api/game/location/BankLocation.java
@@ -10,15 +10,20 @@ import com.runemate.game.api.hybrid.location.Coordinate;
  */
 public enum BankLocation implements Locatable {
     NONE("None",                                         Type.NONE,    new Coordinate(0,    0,    0), new Coordinate(0,    0,    0)),
+    APE_ATOLL_SOUTH("Ape Atoll",                         Type.DEPOSIT, new Coordinate(3047, 3237, 0), new Coordinate(3047, 3237, 0)),
     ARDOUGNE_EAST("Ardougne East",                       Type.FULL,    new Coordinate(2655, 3283, 0), new Coordinate(2655, 3283, 0)),
     BARBARIAN_OUTPOST("Barbarian outpost",               Type.FULL,    new Coordinate(2535, 3572, 0), new Coordinate(2535, 3572, 0)),
     BURGH_DE_ROTT("Burgh de Rott",                       Type.FULL,    new Coordinate(3494, 3211, 0), new Coordinate(3494, 3211, 0)),
     BURTHORPE("Burthorpe",                               Type.FULL,    new Coordinate(2888, 3535, 0), new Coordinate(0,    0,    0)),
     CATHERBY("Catherby",                                 Type.FULL,    new Coordinate(2795, 3439, 0), new Coordinate(2809, 3440, 0)),
+    CLAN_CAMP_FALADOR("Clan camp Falador",               Type.FULL,    new Coordinate(3011, 3355, 0), new Coordinate(-1, -1, 0)),
     DRAYNOR_VILLAGE("Draynor Village",                   Type.FULL,    new Coordinate(3091, 3244, 0), new Coordinate(3093, 3243, 0)),
+    DUEL_ARENA("Duel Arena",                             Type.SPECIAL, new Coordinate(3349, 3238, 0), new Coordinate(3349, 3238, 0)),
     EDGEVILLE("Edgeville",                               Type.FULL,    new Coordinate(3094, 3496, 0), new Coordinate(3094, 3491, 0)),
+    FALADOR_EAST("Falador East",                         Type.FULL,    new Coordinate(3009, 3355, 0), new Coordinate(3009, 3355, 0)),
     FALADOR_WEST("Falador West",                         Type.FULL,    new Coordinate(2945, 3369, 0), new Coordinate(0,    0,    0)),
     FISHING_GUILD("Fishing Guild",                       Type.FULL,    new Coordinate(2584, 3422, 0), new Coordinate(2587, 3419, 0)),
+    GRAND_EXCHANGE("Grand Exchange",                     Type.FULL,    new Coordinate(3151, 3480, 0), new Coordinate(3151, 3480, 0)),
     KARAMJA("Stiles",                                    Type.SPECIAL, new Coordinate(2851, 3143, 0), new Coordinate(3045, 3234, 0)),
     LIVING_ROCK_CAVERNS("Living Rock Caverns",           Type.SPECIAL, new Coordinate(3652, 5114, 0), new Coordinate(0,    0,    0)),
     LUMBRIDGE_CASTLE("Lumbridge Castle",                 Type.FULL,    new Coordinate(3208, 3219, 2), new Coordinate(3208, 3219, 2)),
@@ -33,7 +38,8 @@ public enum BankLocation implements Locatable {
     VARROCK_EAST("Varrock East",                         Type.FULL,    new Coordinate(0,    0,    0), new Coordinate(3253, 3420, 0)),
     VARROCK_WEST("Varrock West",                         Type.FULL,    new Coordinate(3190, 3435, 0), new Coordinate(0,    0,    0)),
     WAIKO("Waiko (The Arc)",                             Type.FULL,    new Coordinate(1831, 11613,0), new Coordinate(0,    0,    0)),
-    WHALES_MAW("Whale's Maw (The Arc)",                  Type.DEPOSIT, new Coordinate(2058, 11781,0), new Coordinate(0,    0,    0));
+    WHALES_MAW("Whale's Maw (The Arc)",                  Type.DEPOSIT, new Coordinate(2058, 11781,0), new Coordinate(0,    0,    0)),
+    WOODCUTTING_GUILD("Woodcutting Guild",               Type.FULL,    new Coordinate(1592, 3476, 0), new Coordinate(1592, 3476, 0));
 
     private final String name;
     private final Type type;
